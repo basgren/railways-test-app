0e238ab96835ce79da17806bfd7c9d5d138630db
Basil Gren <bas.gren@gmail.com>
2014-11-14 01:46:48 +0300

    Add namespaced route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :books
 
+  namespace :api do
+    resources :books, only: %w(index show create update destroy)
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
